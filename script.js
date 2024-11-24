44ba5a00015cf394c72f51a125f9726fe681754b
Jayanta66 <jayhalder18@gmail.com>
2024-11-24 20:09:01 +0530

    add js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Jayanta Halder");
+console.log("Jayanta Halder in console");
+
+var firstName="Jayanta";
+var lastName="Halder";
+console.log(firstName+" "+lastName);
+alert("First Name : "+firstName+" "+"Last Name :"+lastName);
