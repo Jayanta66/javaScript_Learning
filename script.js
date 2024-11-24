9445607d6c0b8199ed16c47a2cc9a7c60461476f
Jayanta66 <jayhalder18@gmail.com>
2024-11-24 20:14:41 +0530

    add js

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ var firstName="Jayanta";
 var lastName="Halder";
 console.log(firstName+" "+lastName);
 alert("First Name : "+firstName+" "+"Last Name :"+lastName);
+
+var FullName = prompt('What is the FullName !!!!');
+console.log(firstName+"  "+lastName);
