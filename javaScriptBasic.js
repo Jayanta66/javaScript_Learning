ed94c554a7bc11ec8047d63bb033eeabe8174392
Jayanta66 <jayhalder18@gmail.com>
2024-11-29 08:31:06 +0530

    JavaScript Basic

---
+++ javaScriptBasic.js
@@ -0,0 +1,15 @@
+
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 20;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = "Java Developer";
+console.log(job);
