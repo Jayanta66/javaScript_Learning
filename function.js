ebd69b19dc00340dcc673112b7c82f0d8c73a9e3
Jayanta66 <jayhalder18@gmail.com>
2024-12-04 18:39:27 +0530

    javaScript

--- function.js
+++ function.js
@@ -8,3 +8,23 @@ function calculateAge(birthYear){
 var ageJohn = calculateAge(1998);
 console.log(ageJohn);
 
+
+
+function yearUntilRetirement(year, firstName){
+
+	var age = calculateAge(year);
+	var retirment = 65 - age;
+
+	if(retirment > 0){
+		console.log(firstName+' retires in '+ retirment + ' years.');
+
+	}else{
+		console.log(firstName+' is already retired.');
+
+	}
+
+}
+
+yearUntilRetirement(1990, 'John');
+yearUntilRetirement(1948, 'Mike');
+yearUntilRetirement(1969, 'John');
