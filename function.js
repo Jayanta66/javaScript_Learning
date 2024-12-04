0f1132b20cc564014d0d1e259e79b1fa5504f73e
Jayanta66 <jayhalder18@gmail.com>
2024-12-04 18:58:21 +0530

    javaScript

--- function.js
+++ function.js
@@ -28,3 +28,35 @@ function yearUntilRetirement(year, firstName){
 yearUntilRetirement(1990, 'John');
 yearUntilRetirement(1948, 'Mike');
 yearUntilRetirement(1969, 'John');
+
+//Function Declaration
+//Function whatDoYouDo(job,firstName){}
+
+
+//Function expression
+var whatDoYouDo = function(job,firstName){
+
+	switch(job){
+
+		case 'teacher':
+			return firstName+' teaches kids how to code';
+
+		case 'driver':
+			return firstName+' drives a cab in Lisbon';
+
+		case 'designer':
+			return firstName+' designs beautiful websites';
+
+
+		default:
+			return firstName+' does something else';
+	}
+
+}
+
+console.log(whatDoYouDo('teacher', 'John'));
+console.log(whatDoYouDo('driver', 'Jane'));
+console.log(whatDoYouDo('designer', 'Jayanta'));
+
+
+
