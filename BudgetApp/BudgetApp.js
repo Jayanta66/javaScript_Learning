f957ce72c0701a9a00111a8d1ca5eddb922743a7
Jayanta66 <jayhalder18@gmail.com>
2024-12-07 11:07:28 +0530

    BudgetApp

--- BudgetApp/BudgetApp.js
+++ BudgetApp/BudgetApp.js
@@ -7,21 +7,3 @@ Variable and data types
 
 var firstName = 'John';
 console.log(firstName);
-
-var lastName = 'Smith';
-var age = 20;
-
-var fullAge = true;
-console.log(fullAge);
-
-var job;
-console.log(job);
-
-job = "Java Developer";
-console.log(job);
-
-console.log("First Name : "+firstName+" "+"Last Name : "+" "+lastName+"Age : "+age);
-
-alert("First Name : "+firstName+" "+"Last Name : "+" "+lastName+"Age : "+age);
-
-prompt("First Name : "+firstName+" "+"Last Name : "+" "+lastName+"Age : "+age)
