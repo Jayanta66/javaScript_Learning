4a073a43562c0e2250fb560c40763b2ecd12018b
Jayanta66 <jayhalder18@gmail.com>
2024-12-11 10:58:08 +0530

    javaScript

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,28 @@
 console.log("Hellow world");
+
+
+//Varriable name conventions
+let firstName ="Matilda";
+let first ="jonas";
+console.log(firstName);
+
+
+//Assignment 1
+console.log("Assignment 1")
+console.log("Values and Variables Declare variables called country, continent and population and assign their values according to your own country.Log their values to the console.");
+
+let country = "India";
+let continent = "Asia";
+let population = 10000000;
+
+console.log(country,continent,population);
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+console.log(typeof true);
+console.log(typeof 23);
+console.log(typeof "Jonas");
