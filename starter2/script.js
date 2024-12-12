7fddf20f2e57c97ef794f940e68de9cb16c83eb5
Jayanta66 <jayhalder18@gmail.com>
2024-12-12 15:05:30 +0530

    javaScript

--- starter2/script.js
+++ starter2/script.js
@@ -102,3 +102,31 @@ function fruitProcessor(apples, oranges){
 }
 
 console.log(fruitProcessor(2,3));
+
+
+console.log(" ");
+console.log("Coding Challange : 1");
+console.log("#############################################");
+console.log(" ");
+
+const calcAverage = (a,b,c) => (a+b+c) /3;
+console.log(calcAverage(3,4,5));
+
+const scoreDolphins = calcAverage(44,23,71);
+const scoreKoalas = calcAverage(65,54,49);
+console.log(scoreDolphins,scoreKoalas);
+
+const checkWinner = function(avgDolphins,avgKoalas){
+    if(avgDolphins >= 2 * avgKoalas){
+        console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+    }else if(avgKoalas >= 2 * avgDolphins){
+        console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+
+    }else{
+        console.log("No team wins...");
+    }
+}
+
+checkWinner(scoreDolphins,scoreKoalas);
+
+checkWinner(576,111);
